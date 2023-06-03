efc20c354129d568d63017ca0adabe508ac1b6db
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-06-02 23:07:19 -0500

    Create html_to_markdown.py

---
+++ html_to_markdown.py
@@ -0,0 +1,46 @@
+from html.parser import HTMLParser
+
+
+class HTMLToMarkdownParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.markdown = ""
+        self.links = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "p":
+            self.markdown += "\n\n"
+        elif tag == "strong" or tag == "b":
+            self.markdown += "**"
+        elif tag == "em" or tag == "i":
+            self.markdown += "_"
+        elif tag == "a":
+            attrs_dict = dict(attrs)
+            if "href" in attrs_dict:
+                self.links.append({"text": "", "href": attrs_dict["href"]})
+
+    def handle_endtag(self, tag):
+        if tag == "p":
+            self.markdown += "\n\n"
+        elif tag == "strong" or tag == "b":
+            self.markdown += "**"
+        elif tag == "em" or tag == "i":
+            self.markdown += "_"
+        elif tag == "a":
+            link = self.links.pop()
+            if link["text"]:
+                self.markdown += f"[{link['text']}]({link['href']})"
+            else:
+                self.markdown += link["href"] + " "
+
+    def handle_data(self, data):
+        if self.links:
+            self.links[-1]["text"] += data
+        else:
+            self.markdown += data
+
+
+def html_to_markdown(html):
+    parser = HTMLToMarkdownParser()
+    parser.feed(html)
+    return parser.markdown
