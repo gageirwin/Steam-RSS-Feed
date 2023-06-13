08a30733c3efc12e50688932c50722552e241b93
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-06-13 03:29:14 -0500

    Update steam_rss.py
    added error handling for requests.exceptions.ReadTimeout

--- steam_rss.py
+++ steam_rss.py
@@ -1,5 +1,6 @@
 import os
 import time
+import traceback
 from datetime import datetime
 from xml.etree import ElementTree
 
@@ -36,6 +37,9 @@ def main():
     steam_icon = "https://cdn.cloudflare.steamstatic.com/valvesoftware/images/about/steam_logo.png"
     webhook = SyncWebhook.from_url(args.webhook)
 
+    max_retries = 10
+    wait_time = 30
+
     feeds = []
     feeds += [f"https://steamcommunity.com/ogg/{appid}/rss/" for appid in args.appid]
     feeds += [f"https://steamcommunity.com/groups/{group}/rss/" for group in args.group]
@@ -47,72 +51,102 @@ def main():
                 old_feed = [line.strip() for line in f.readlines()]
 
         for feed_url in feeds:
-            print(f"Getting feed for {feed_url}")
-            response = requests.get(feed_url)
+            retries = 0
+            while True:
+                try:
+                    print(f"Getting feed for {feed_url}")
+                    response = requests.get(feed_url)
 
-            if not response.ok:
-                print(f"Error: {response.status_code} | {response.reason} Getting feed")
-                embed_dict = {
-                    "author": {
-                        "name": username,
-                        "icon_url": steam_icon,
-                    },
-                    "title": f"Error: {response.status_code} | {response.reason}",
-                    "description": f"{feed_url}",
-                    "color": RED,
-                }
-                webhook.send(
-                    username=username,
-                    avatar_url=steam_icon,
-                    embed=Embed.from_dict(embed_dict),
-                )
-                continue
+                    if not response.ok:
+                        print(
+                            f"Error: {response.status_code} | {response.reason} Getting feed"
+                        )
+                        embed_dict = {
+                            "author": {
+                                "name": username,
+                                "icon_url": steam_icon,
+                            },
+                            "title": f"Error: {response.status_code} | {response.reason}",
+                            "description": f"{feed_url}",
+                            "color": RED,
+                        }
+                        webhook.send(
+                            username=username,
+                            avatar_url=steam_icon,
+                            embed=Embed.from_dict(embed_dict),
+                        )
+                        continue
 
-            root = ElementTree.fromstring(response.text)
-            channel = root.find("channel")
+                    root = ElementTree.fromstring(response.text)
+                    channel = root.find("channel")
 
-            feed_title = channel.find("title").text
-            feed_link = channel.find("link").text
-            game_thumbnail = ""
-            if channel.find("image") != None:
-                game_thumbnail = channel.find("image").find("url").text
+                    feed_title = channel.find("title").text
+                    feed_link = channel.find("link").text
+                    game_thumbnail = ""
+                    if channel.find("image") != None:
+                        game_thumbnail = channel.find("image").find("url").text
 
-            for item in reversed(list(channel.findall("item"))):
-                guid = item.find("guid").text
-                author = item.find("author")
-                if author == None or guid in old_feed:
+                    for item in reversed(list(channel.findall("item"))):
+                        guid = item.find("guid").text
+                        author = item.find("author")
+                        if author == None or guid in old_feed:
+                            continue
+
+                        meta_tags = get_opengraph_meta_tags(guid)
+
+                        date = datetime.strptime(
+                            item.find("pubDate").text, "%a, %d %b %Y %H:%M:%S %z"
+                        ).astimezone()
+
+                        embed = {
+                            "author": {
+                                "name": feed_title,
+                                "url": f"{feed_link}/announcements",
+                                "icon_url": "",
+                            },
+                            "title": item.find("title").text,
+                            "url": guid,
+                            "color": GREEN,
+                            "description": meta_tags.get("description", ""),
+                            "thumbnail": {"url": game_thumbnail},
+                            "image": {"url": meta_tags.get("image", "")},
+                            "timestamp": date.isoformat(),
+                        }
+
+                        if old_feed != [] or args.force_old:
+                            webhook.send(
+                                username=username,
+                                avatar_url=steam_icon,
+                                embed=Embed.from_dict(embed),
+                            )
+
+                        with open(args.archive, "a+") as f:
+                            f.write(guid + "\n")
                     continue
-
-                meta_tags = get_opengraph_meta_tags(guid)
-
-                date = datetime.strptime(
-                    item.find("pubDate").text, "%a, %d %b %Y %H:%M:%S %z"
-                ).astimezone()
-
-                embed = {
-                    "author": {
-                        "name": feed_title,
-                        "url": f"{feed_link}/announcements",
-                        "icon_url": "",
-                    },
-                    "title": item.find("title").text,
-                    "url": guid,
-                    "color": GREEN,
-                    "description": meta_tags.get("description", ""),
-                    "thumbnail": {"url": game_thumbnail},
-                    "image": {"url": meta_tags.get("image", "")},
-                    "timestamp": date.isoformat(),
-                }
-
-                if old_feed != [] or args.force_old:
-                    webhook.send(
-                        username=username,
-                        avatar_url=steam_icon,
-                        embed=Embed.from_dict(embed),
+                except requests.exceptions.ReadTimeout as e:
+                    retries += 1
+                    if retries > max_retries:
+                        print("All retires failed.")
+                        embed_dict = {
+                            "author": {
+                                "name": username,
+                                "icon_url": steam_icon,
+                            },
+                            "title": "Error: requests.exceptions.ReadTimeout",
+                            "description": f"All retries failed.\n{feed_url} has been skipped.",
+                            "color": RED,
+                        }
+                        webhook.send(
+                            username=username,
+                            avatar_url=steam_icon,
+                            embed=Embed.from_dict(embed_dict),
+                        )
+                        break
+                    print("Error occurred:", e)
+                    print(
+                        f"Retrying in {wait_time*retries} seconds. ({retries}/{max_retries})"
                     )
-
-                with open(args.archive, "a+") as f:
-                    f.write(guid + "\n")
+                    time.sleep(wait_time * retries)
 
         if not args.continuous:
             break
